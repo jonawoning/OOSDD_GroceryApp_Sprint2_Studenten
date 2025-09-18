1d3425750dbf1f3693ed61cae7a8e006de8c772d
jonawoning <jonawoningyt@gmail.com>
2025-09-18 16:02:11 +0200

    Client repository fix & Client class fix
    - Toevoegen van de NewId en de properties publiek maken
    - Client repository functies veranderen

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,11 +3,15 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string EmailAddress { get; set; }
-        private string Password { get; set; }
+        public int NewId { get; set; }
+        // public string NewName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
         
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
+            // NewId = id;
+            // NewName = name;
             EmailAddress = emailAddress;
             Password = password;
         }
